43edc48036f12471a0ca4c0a31a8c8e1491fb076
saravanamuthukumaran <saravana.murugaesan@outlook.com>
2026-07-08 14:56:14 +0530

    Add files via upload

---
+++ Week 1/Day1Assignment.js
@@ -0,0 +1,19 @@
+var firstName="Saravana"
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = `BeQiSoft`
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber=9751812341
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation=true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+        
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
