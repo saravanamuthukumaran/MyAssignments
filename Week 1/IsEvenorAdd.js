80eba274dc11fe490418cc35c91d2723cc62965f
saravanamuthukumaran <saravana.murugaesan@outlook.com>
2026-07-13 10:01:29 +0530

    Add files via upload

---
+++ Week 1/IsEvenorAdd.js
@@ -0,0 +1,11 @@
+function isOddOrEven() {
+    i=7;
+    if (i%2==0) {
+        console.log("Even Number");
+    }
+    else{
+        console.log("Odd Number");
+    }
+}
+isOddOrEven()
+console.log("Declared Number is",i);
