4ca763d470cc654b3cedbd9a6c556bb077eb1c27
saravanamuthukumaran <saravana.murugaesan@outlook.com>
2026-07-13 09:48:42 +0530

    Add files via upload

---
+++ Week 1/PrimitiveDataType.js
@@ -0,0 +1,19 @@
+let userName="Saravana"
+console.log(userName);
+console.log(typeof userName);
+
+let organization="BeQiSoft"
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber=9751812341
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+let knowsAutomation=true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
