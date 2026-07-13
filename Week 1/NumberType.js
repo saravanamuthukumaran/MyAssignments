42670a50d646a0ba4d8a090fbd4faffd9d36b055
saravanamuthukumaran <saravana.murugaesan@outlook.com>
2026-07-13 09:53:12 +0530

    Add files via upload

---
+++ Week 1/NumberType.js
@@ -0,0 +1,14 @@
+function number() {
+     i=0;
+    if (i>1) {
+        console.log(" positive number"); 
+    }
+    else if (i<2) {
+        console.log("Negative Number");
+    }
+    else{
+        console.log("Netural number");
+    }  
+}
+number()
+console.log(i);
